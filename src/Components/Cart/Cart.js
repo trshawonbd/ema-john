b0515809e4dd88d6583a9b68a6163a7001dff526
Your Name <you@example.com>
2022-03-22 23:03:21 +0200

    order calculation  added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
-    const cart = props.cart
+    const {cart} = props
+
+    let totalPrice = 0;
+    let shippingCharge = 0;
+    for (const product of cart){
+        totalPrice = totalPrice + product.price;
+        shippingCharge = shippingCharge + product.shipping; 
+
+    }
+    let tax = (totalPrice + shippingCharge) * 0.1;
+    let grandTotal = totalPrice + shippingCharge + parseFloat(tax)
+     
+
     return (
-        <div>
+        <div className='cart-info'>
             <h2>Order Summary</h2>
-            <h4>Selected Items: {cart.length}</h4>
+            <h5>Selected Items: {cart.length}</h5>
+            <h5>Total Price: ${totalPrice}</h5>
+            <h5>Total Shipping Charge: ${shippingCharge}</h5>
+            <h5>Tax: ${tax.toFixed(2)}</h5>
+            <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
